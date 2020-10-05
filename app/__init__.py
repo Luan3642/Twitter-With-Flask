a28ecbef999cd7bbb8a29f392f8f78d3eb0ab744
luan3642 <luan3642@hotmail.com>
2020-10-05 17:24:21 -0300

    configuracoes de rotas

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand 
 
 # instância do flask, essa instância recebe uma variável
 # essa variável vai controlar a aplicação inteira 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
 
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from app.controllers import default 
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,17 @@
 from  app import app 
 
 
+@app.route("/index")
 @app.route("/") 
 def index():
     return "hello world"
+
+
+#podemos passar parametros as rotas
+@app.route("/test", defaults={'name': None}) 
+@app.route("/test/<name>")
+def teste(name):
+    if name:
+        return "Ola, %s" % name
+    else:
+        return "Olá, usuário"
