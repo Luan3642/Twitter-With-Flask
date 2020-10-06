eec6bdfab8700d23600d4fe056529c7857b2576a
luan3642 <luan3642@hotmail.com>
2020-10-06 11:53:47 -0300

    form feito

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,9 @@
 from  app import app 
 from flask import render_template
 
+
+from app.models.forms import LoginForm
+
 @app.route("/index")
 @app.route("/") 
 def index():
@@ -16,6 +19,13 @@ def index():
 #         return "Olá, usuário"
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
+    return render_template("login.html", form=form)
